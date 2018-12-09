c26c581ec03f216055327bfb66e2fc6e18c43f04
William Sheu <sheu.william@gmail.com>
2018-12-09 13:45:12 -0800

    Update arduino code

--- arduino/spherebot.ino
+++ arduino/spherebot.ino
@@ -4,37 +4,105 @@ SoftwareSerial ble(13, 12);
 
 const int NUM_OUTPUTS = 4;
 const int MOTOR_PINS[] = {3, 5, 6, 9};
+const int MOTORS[2][2] = {
+  {
+    MOTOR_PINS[0], MOTOR_PINS[1]
+  },
+  {
+    MOTOR_PINS[2], MOTOR_PINS[3]
+  }
+};
+const byte ZERO[] = {0, 0, 0, 0};
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Begin");
   ble.begin(9600);
-  ble.println("Begin");
 
   for (int i = 0; i < NUM_OUTPUTS; i++) {
     pinMode(MOTOR_PINS[i], OUTPUT);
     digitalWrite(MOTOR_PINS[i], LOW);
   }
+  ble.write(ZERO, 4);
 }
 
-bool motorStates[] = {false, false, false, false};
+bool motorStates[2] = {false, false};
+union BleData {
+  struct {
+    unsigned long seq;
+    byte currentMotor;
+    float magnitude;
+  } data;
+  byte bytes[9];
+};
+union BleData bleData;
 
 void loop() {
   if (ble.available() > 0) {
-    int motor = ble.parseInt();
-    Serial.println(motor);
-    if (motor < 0 || motor >= NUM_OUTPUTS) {
-      ble.println("Invalid output");
+    ble.readBytes(bleData.bytes, 9);
+    bool start = true;
+    for (int i = 0; i < 9; i++) {
+      if (bleData.bytes[i] != 0xFF) {
+        start = false;
+      }
+    }
+    if (start) {
+      Serial.println("Start!");
+      ble.write(ZERO, 4);
+      digitalWrite(MOTORS[0][0], LOW);
+      digitalWrite(MOTORS[0][1], LOW);
+      digitalWrite(MOTORS[1][0], LOW);
+      digitalWrite(MOTORS[1][1], LOW);
+      return;
+    }
+    bool end = true;
+    for (int i = 0; i < 9; i++) {
+      if (bleData.bytes[i] != 0xEE) {
+        end = false;
+      }
+    }
+    if (start) {
+      Serial.println("End!");
+      ble.write(ZERO, 4);
+      digitalWrite(MOTORS[0][0], LOW);
+      digitalWrite(MOTORS[0][1], LOW);
+      digitalWrite(MOTORS[1][0], LOW);
+      digitalWrite(MOTORS[1][1], LOW);
+      return;
+    }
+    ble.write(bleData.bytes, 4);
+    for (int i = 0; i < 9; i++) {
+      Serial.print(bleData.bytes[i], HEX);
+      Serial.print(" ");
+    }
+    Serial.println();
+    Serial.print("Seq: ");
+    Serial.println(bleData.data.seq);
+    Serial.print("Motor: ");
+    Serial.println(bleData.data.currentMotor);
+    Serial.print("Mag: ");
+    Serial.println(bleData.data.magnitude);
+    if (bleData.data.currentMotor != 0 && bleData.data.currentMotor != 1) {
+      Serial.println("Invalid motor selection!");
+      return;
+    }
+    if (bleData.data.magnitude > 1.0 || bleData.data.magnitude < -1.0) {
+      Serial.println("Invalid magnitude!");
       return;
     }
 
-    bool newState = !motorStates[motor];
-    motorStates[motor] = newState;
-    digitalWrite(MOTOR_PINS[motor], newState ? HIGH : LOW);
-
-    ble.print("Toggling motor ");
-    ble.print(motor);
-    ble.print(" to ");
-    ble.println(newState);
+    if (bleData.data.magnitude > 0.0) {
+      if (!motorStates[bleData.data.currentMotor]) {
+        digitalWrite(MOTORS[bleData.data.currentMotor][1], LOW);
+        motorStates[bleData.data.currentMotor] = true;
+      }
+      analogWrite(MOTORS[bleData.data.currentMotor][0], bleData.data.magnitude * 255);
+    } else {
+      if (motorStates[bleData.data.currentMotor]) {
+        digitalWrite(MOTORS[bleData.data.currentMotor][0], LOW);
+        motorStates[bleData.data.currentMotor] = false;
+      }
+      analogWrite(MOTORS[bleData.data.currentMotor][1], bleData.data.magnitude * -255);
+    }
   }
 }
